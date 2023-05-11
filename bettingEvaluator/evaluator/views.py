b78a326dfa7647777ae8255c84336e9f4311cdf5
abriel0402 <abchprod@gmail.com>
2023-05-11 19:50:29 -0400

    added projections and stat averages

--- bettingEvaluator/evaluator/views.py
+++ bettingEvaluator/evaluator/views.py
@@ -5,6 +5,9 @@ import requests
 from nba_api.stats.static import players
 from nba_api.stats.endpoints import playergamelog
 
+from nba_api.stats.endpoints import *
+
+
 
 PLAYERS = players.get_players()
 
@@ -23,7 +26,6 @@ playerTagsRebounds = rebsLinesSoup.find_all("span", class_="sportsbook-row-name"
 lineTagsPoints = ptsLinesSoup.find_all("span", class_="sportsbook-outcome-cell__line")
 lineTagsAssists = astsLinesSoup.find_all("span", class_="sportsbook-outcome-cell__line")
 lineTagsRebounds = rebsLinesSoup.find_all("span", class_="sportsbook-outcome-cell__line")
-
 
 
 
@@ -84,7 +86,40 @@ def getHitRates(leg, n):
             hitCount = hitCount + 1
     
     hitRateN = str(int((hitCount/n)*100))+"%"
-    return [hitRateN, lastN]
+    if (hitCount/n)*100 >= float(85):
+        color = "#1ebf02"
+    elif (hitCount/n)*100 >= float(70):
+        color = "#127301"
+    elif (hitCount/n)*100 >= float(50):
+        color = "#075901"
+    elif (hitCount/n)*100 >= float(38):
+        color = "#591101"
+    elif (hitCount/n)*100 >= float(25):
+        color = "#870d01"
+    else:
+        color = "#e31502"
+    
+    return [hitRateN, lastN, color]
+
+
+# Decides whether to go over, under, or pass
+def getDecision(last5, last10, last20):
+    hitRate5 = int(last5[0][:-1])
+    hitRate10 = int(last10[0][:-1])
+    hitRate20 = int(last20[0][:-1])
+    average = (hitRate5 + hitRate10 + hitRate20)/3
+    if average >= 65:
+        decision = "OVER"
+        color = "btn btn-success"
+    elif average <= 35:
+        decision = "UNDER"
+        color = "btn btn-danger"
+    else:
+        decision = "PASS"
+        color = "btn btn-dark"
+    return [decision, color]
+
+
 
 
 # Create your views here.
@@ -107,38 +142,46 @@ def rebounds(request):
         "legs": REBOUNDS_LEGS,
     })
 
-def player(request, playerID, stat):
-    if stat == "PTS":
-        for leg in POINTS_LEGS:
-            if str(leg.playerID) == str(playerID):
-                leg = leg
-                statTxt = "pts"
-                break
-    if stat == "AST":
-        for leg in ASSISTS_LEGS:
-            if str(leg.playerID) == str(playerID):
-                leg = leg
-                statTxt = "asts"
-                break
-    if stat == "REB":
-        for leg in REBOUNDS_LEGS:
-            if str(leg.playerID) == str(playerID):
-                leg = leg
-                statTxt = "rebs"
-                break
-    
 
+def player(request, playerID, stat):
+
+    if stat == "PTS":
+        CURR_LEGS = POINTS_LEGS
+        statTxt = "pts"
+        statPerGame = "ppg"
+    elif stat == "AST":
+        CURR_LEGS = ASSISTS_LEGS
+        statTxt = "asts"
+        statPerGame = "apg"
+    elif stat == "REB":
+        CURR_LEGS = REBOUNDS_LEGS
+        statTxt = "rebs"
+        statPerGame = "rpg"
+
+    for leg in CURR_LEGS:
+        if str(leg.playerID) == str(playerID):
+            leg = leg
+            break
+    
+    
+    #  Hit Rates
     hitRates = getHitRates(leg, 20)
+    color20 = hitRates[2]
     leg.last20 = hitRates[1]
     hitRate20 = hitRates[0]
+    
 
     hitRates = getHitRates(leg, 10)
+    color10 = hitRates[2]
     leg.last10 = hitRates[1]
     hitRate10 = hitRates[0]
 
     hitRates = getHitRates(leg, 5)
+    color5 = hitRates[2]
     leg.last5 = hitRates[1]
     hitRate5 = hitRates[0]
+
+
 
     #Get Season Hit Rate
     hitCount = 0
@@ -149,6 +192,28 @@ def player(request, playerID, stat):
             hitCount = hitCount + 1
     hitRateSzn = str(int((hitCount/len(leg.season))*100))+"%"
         
+    # Get Averages
+    total = 0
+    for stat in leg.last5:
+        total = total + stat
+    average5 = round((total/5), 1)
+    total = 0
+    for stat in leg.last10:
+        total = total + stat
+    average10 = round((total/10), 1)
+    total = 0
+    for stat in leg.last20:
+        total = total + stat
+    average20 = round((total/20), 1)
+    total = 0
+    for stat in leg.season:
+        total = total + stat
+    averageSeason = round((total/len(leg.season)), 1)
+
+    decisionList = getDecision(getHitRates(leg, 5),getHitRates(leg, 10),getHitRates(leg, 20))
+    decision = decisionList[0]
+    color = decisionList[1]
+    
 
     return render(request, "evaluator/player.html", {
         "leg": leg,
@@ -157,4 +222,15 @@ def player(request, playerID, stat):
         "hitRate20": hitRate20,
         "hitRateSzn": hitRateSzn,
         "stat": statTxt,
+        "statPerGame": statPerGame,
+        "average5": average5,
+        "average10": average10,
+        "average20": average20,
+        "averageSeason": averageSeason,
+        "color5": color5,
+        "color10": color10,
+        "color20": color20,
+        "decision": decision,
+        "color": color,
+
     })
